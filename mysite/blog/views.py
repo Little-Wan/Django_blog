7a17cf4963fa0715911dde2d68776d1fd12a7df9
wanyuyang <1178984846@qq.com>
2021-01-06 23:35:29 +0800

    Bootstrap响应式布局（1）

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response,get_object_or_404
 def blog_list(request):
     context = {}
     context['blogs'] = Blog.objects.all()
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response("blog/blog_list.html",context)
 
 def blog_detail(request, blog_pk):
